764768f8c6e7ab2e838c8bd5b4ee77160ee987cb
Brittany Taylor <hopkins.britt@gmail.com>
2020-01-09 22:45:44 -0600

    getting three numbers but problem with innerHTML

--- assets/logic.js
+++ assets/logic.js
@@ -1,13 +1,37 @@
 //The array that the random numbers function will select from
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-//The array that 
-var mysticResult = [0, 1, 2];
 
-function getRandom(arr) {
-    var randomNumber = Math.floor(Math.random() * arr.length);
-    console.log(randomNumber);
-    document.getElementById("mysticResult").innerHTML = randomNumber
-  }
+// function getRandom(arr) {
+//     var randomNumber = Math.floor(Math.random() * arr.length);
+//     console.log(randomNumber);
+//     return randomNumber;
+//   }
 
-getRandom(numbers);
+var mysticResult = [];
+
+function clearResult(){
+    mysticResult = [];
+}
+
+function getMysticResult(){
+
+    if(mysticResult.length < 3){
+        for(var i = 0; i < 3; i++){
+            var randomNumber = Math.floor(Math.random() * numbers.length);
+            mysticResult.push(randomNumber); 
+        }
+        
+        console.log(mysticResult);
+    
+        document.getElementById("mysticResult").innerHTML = mysticResult;
+
+    } else {
+        clearResult();
+        getMysticResult();
+    }
+    
+    
+}
+
+getMysticResult();
 
